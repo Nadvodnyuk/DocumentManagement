08bca65ec5245218f2557edb537c32cbced73e4b
ynadvodnyuk <rimsky.yana@yandex.com>
2024-09-18 22:31:14 +0300

    NewDoc form completed

--- frontend/src/pages/newDoc/NewDoc.vue
+++ frontend/src/pages/newDoc/NewDoc.vue
@@ -1,29 +1,121 @@
-<script setup></script>
-
 <template>
-  <div>
-    <div class="sub_header"></div>
-    <div>
-      <p>Создать новый документ</p>
-    </div>
+  <div class="wrapper">
+    <h3 class="title">Создать новый документ</h3>
+    <form @submit.prevent="submitForm" enctype="multipart/form-data">
+      <div class="mb-3">
+        <label for="documentData.documentName" class="form-label"
+          >Название документа</label
+        >
+        <input
+          type="text"
+          v-model="documentData.documentName"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <div class="mb-3">
+        <label for="documentData.author" class="form-label">Автор</label>
+        <input
+          type="text"
+          v-model="documentData.author"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <div class="mb-3">
+        <label for="documentData.documentIntroNumber" class="form-label"
+          >Входящий номер документа</label
+        >
+        <input
+          type="text"
+          v-model="documentData.documentIntroNumber"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <div class="mb-3">
+        <label for="formattedDateIntro" class="form-label"
+          >Дата поступления (автоматически)</label
+        >
+        <input
+          type="text"
+          v-model="formattedDateIntro"
+          class="form-control"
+          disabled
+        />
+      </div>
+
+      <div class="mb-3">
+        <label for="file" class="form-label">Загрузить файл</label>
+        <input
+          type="file"
+          @change="handleFileChange"
+          class="form-control"
+          required
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Отправить</button>
+    </form>
   </div>
 </template>
 
 <script>
-import { useCatalog } from "../../store/catalog.js";
-import { mapState, mapActions } from "pinia";
 import NewDocDataService from "../../services/NewDocDataService";
 
 export default {
   name: "NewDoc",
   data() {
-    return {};
+    return {
+      documentData: {
+        documentName: "",
+        author: "",
+        documentIntroNumber: "",
+        dateIntro: new Date(),
+      },
+      file: null,
+    };
   },
   computed: {
-    ...mapState(useCatalog, []),
+    formattedDateIntro() {
+      return this.documentData.dateIntro.toLocaleString("ru-RU", {
+        day: "2-digit",
+        month: "2-digit",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+      });
+    },
   },
   methods: {
-    ...mapActions(useCatalog, []),
+    handleFileChange(event) {
+      this.file = event.target.files[0];
+    },
+    async submitForm() {
+      try {
+        const documentToSubmit = {
+          ...this.documentData,
+          dateIntro: this.documentData.dateIntro.toISOString(),
+        };
+        await NewDocDataService.createDocument(this.file, documentToSubmit);
+        alert("Документ успешно создан");
+      } catch (error) {
+        console.error("Ошибка при создании документа:", error);
+        alert("Ошибка при создании документа");
+      }
+    },
+    updateDateIntro() {
+      this.documentData.dateIntro = new Date();
+    },
+  },
+  mounted() {
+    this.interval = setInterval(this.updateDateIntro, 1000);
+  },
+  beforeDestroy() {
+    clearInterval(this.interval);
   },
 };
 </script>
