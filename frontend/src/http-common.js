7407d1c5f940971ce2472ecaef5595c653d3f49e
ynadvodnyuk <rimsky.yana@yandex.com>
2024-09-19 15:32:03 +0300

    port change

--- frontend/src/http-common.js
+++ frontend/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9090/",
+  baseURL: "http://localhost:8080/",
   withCredentials: true,
   headers: {
     // "Content-type": "application/json",
